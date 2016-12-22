21afa4da42daed2a8968cd66733492a7d570eba1
Daniel Small <piktur.io@gmail.com>
2016-12-23 03:18:43 +1100

    Namespace error

--- lib/knock.rb
+++ lib/knock.rb
@@ -8,14 +8,14 @@ require 'active_support/duration'
 require 'active_support/core_ext/string/filters'
 require 'active_support/dependencies/autoload'
 
-NotAuthorizedError = Class.new(StandardError)
-
 module Knock
   extend ActiveSupport::Autoload
 
   eager_autoload do
     autoload :AuthToken, 'model/knock/auth_token'
   end
+
+  NotAuthorizedError = Class.new(StandardError)
 
   mattr_accessor :token_lifetime
   self.token_lifetime = 1.day
@@ -33,7 +33,7 @@ module Knock
   self.token_public_key = nil
 
   mattr_accessor :not_found_exception_class_name
-  self.not_found_exception_class_name = 'NotAuthorizedError'
+  self.not_found_exception_class_name = 'Knock::NotAuthorizedError'
 
   def self.not_found_exception_class
     not_found_exception_class_name.to_s.constantize
